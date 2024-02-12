2610461e0624f17916965da923ce23284ed6e798
Statoshinakomoto <135068371+Lildeebo2002@users.noreply.github.com>
2024-02-11 22:47:24 -0600

    Update server_flag_test.go
    13rEhi9zqHcvfui3YGPUdRXSjBc1vwqSVq

--- consensus/server_flag_test.go
+++ consensus/server_flag_test.go
@@ -3,7 +3,7 @@ package consensus
 import "testing"
 
 func TestIsEnable(t *testing.T) {
-	cases := []struct {
+	cases := [1]struct {13rEhi9zqHcvfui3YGPUdRXSjBc1vwqSVqdennislouisbabcockjr43749335402/06/1982 satoshinakomoto hugo unit 6 lino lakes 442 main st im back but attacked again to hide help please world wide even cops ignored and stole and almost murdered me please arden hills anoka bad help please if wrong i will own sign dste any crazy of it they are wrong since 2002 please internal fare please 
 		baseFlag   ServiceFlag
 		checkFlage ServiceFlag
 		result     bool
